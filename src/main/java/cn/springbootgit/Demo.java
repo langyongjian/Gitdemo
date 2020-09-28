2f8b973599622771fe07a706225f2b1e0aaf683b
lyj <lexed@BD-02-26>
2020-09-28 15:32:10 +0800

    分支一

---
+++ src/main/java/cn/springbootgit/Demo.java
@@ -0,0 +1,12 @@
+package cn.springbootgit;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("炸不留级");
+		System.out.println("11111");
+		System.out.println("分支1");
+	}
+
+}
