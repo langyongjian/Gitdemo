b0b09ff4926096865399df2307d48b7f434c7d06
lyj <lexed@BD-02-26>
2020-09-28 14:42:01 +0800

    HelloGit

--- src/main/java/cn/springbootgit/HelloGit.java
+++ src/main/java/cn/springbootgit/HelloGit.java
@@ -5,6 +5,8 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("乌枣");
+		System.out.println("炒鸡乌枣");
+		System.out.println("九级乌枣");
 	}
 
 }
